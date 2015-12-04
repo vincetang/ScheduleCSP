4294d282ae7bb47506abb8057a0a7292d0989f7f
vincetang <tang.vincent13@gmail.com>
2015-12-04 13:22:25 -0500

    added descriptions, fixed classes

--- schedule_csp2.py
+++ schedule_csp2.py
@@ -15,7 +15,75 @@ Variables:
 
 Constraints:
 
+------------------
+Example:
+
+# list of appointments
+Appointments = [apt1, apt2, apt3, apt4]
+
+Appointment objects are intiialized with [start_time, end_time, procedures]
+    Examples:
+    apt1 = [2, 4, [p1]]
+    apt2 = [2, 6, [p2, p3]]
+    apt3 = [1, 6, [p2]]
+    apt4 = [5, 6, [p3]]
+
+Procedures are initialized with [procedure_name, duration, staff_reqs, resources]
+    Examples:
+    p1 = ['flu shot', 1, ['doctor'], [r1, r2]]
+    p2 = ['measure bp', 1, [], [r3, r4]]
+    p3 = ['diagnose', 2, ['doctor'], []]
+
+Resources are initialized with [name, qty_total]
+    Examples:
+    r1 = ['needle', 3]
+    r2 = ['bandaid', 5]
+    r3 = ['bp monitor', 1]
+    r4 = ['stethascope', 2]
+
+Constraints
+    #TODO
+
+Scheduling:
+    - A schedule is a 5x5 matrix. Each column represents a day, each row represents an hour
+    - An appointment can be scheduled on any day but must be scheduled within the hours of start_time and end_time
+      and must satisfy all constraints listed above
+    - row[i] represents the hour of i to i+1. For example, the first row corresponds to times starting at 1 and
+      ending at 2
+      Examples:
+      [0,0] represents the first day from the first hour to the second hour. An appointment with start_time
+      1 and end_time 2 would fit into [0,0], [0,1], [0,2], [0,3], [0,4] (i.e., any slot in the first row)
+
+      An appointment with start_time 2 and end_time 4 and total_ duration 1 could fit into any column and take
+      any slot in the 2nd or 3rd row
+
+      An appointment wit start_time 2 and end_time 4 and total_duration 2 would take up 2 slots. It could fit in
+      any column but would take up BOTH rows 2 and 3.
+
+      Suppose we have the following appointments:
+      Note appointments are defined with [start_time, end_time, procedures]. Suppose all procedures in this example
+      have a duration of 1 hour.
+      apt1 = [1, 2, [p1]]
+      apt2 = [3, 4, [p3]]
+      apt3 = [1, 5, [p1,p2,p3,p4]]
+      apt4 = [1, 5, [p2]]
+
+      One possible schedule would look like this
+      [ [apt1],[],[],[apt3],[    ],
+        [    ],[],[],[apt3],[    ],
+        [apt2],[],[],[apt3],[apt4],
+        [    ],[],[],[apt3],[    ],
+        [    ],[],[],[apt3],[    ]]
+
+      Another valid schedule for the same appointments would be
+      [ [apt4],[apt3],[],[],[apt1],
+        [    ],[apt3],[],[],[    ],
+        [apt2],[apt3],[],[],[    ],
+        [    ],[apt3],[],[],[    ],
+        [    ],[apt3],[],[],[    ]]
+
 '''
+
 class Appointment:
 
     ''' An Appointment has the following attributes:
@@ -29,28 +97,23 @@ class Appointment:
         starttime and endtime. However, if the appointment cannot be completed
         by endtime, we must rebook the appointment.
     '''
-    def __init__(self, starttime, endtime, duration, procedure):
-        self.starttime = starttime
-        self.endtime = endtime
-        self.duration = duration
-        self.procedure = procedure # a list of procedures
+    def __init__(self, start_time, end_time, procedures):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.procedures = procedures # a list of procedures
 
-
-    def get_starttime(self):
-        '''returns this appointments start time'''
-        return self.starttime
-
-    def get_endtime(self):
-        '''returns this appointments end time'''
-        return self.endtime
-
-    def get_duration(self):
+    def get_total_duration(self):
         '''return this apopintments duration'''
-        return self.duration
+        total_duration = 0
+        for proc in self.procedures:
+            total_duration += proc.duration
+
+        return total_duration
+
 
     def can_be_completed(self, time):
         '''Returns true if appointment can be completed at time'''
-        if (time >= self.starttime) and (self.duration + time <= self.endtime):
+        if (time >= self.start_time) and (self.get_total_duration() + time <= self.end_time):
             return True
         return False
 
@@ -64,29 +127,12 @@ class Procedure:
             resources: the required resources to complete this procedure
             inprogress: true if procedure is being performed (resources in use), false otherwise
     '''
-    def __init(self, procedure_name, actions, pos_reqs, resources ):
+    def __init(self, procedure_name, duration, staff_reqs, resources ):
         self.name = procedure_name
-        self.actions = actions
-        self.pos_reqs = pos_reqs
+        self.duration = duration
+        self.staff_reqs = staff_reqs
         self.resources = resources
         self.inprogress = False
-
-    def get_actions(self):
-        '''returns the actions in done in this procedure, a list of ('name', duration) tuples'''
-        return self.actions
-    def get_required_positions(self):
-        '''returns the list of staff position requirements for this procedure'''
-        return self.pos_reqs
-
-    def get_resources(self):
-        '''returns the list of resources that this procedure uses'''
-
-    def get_total_duration(self):
-        '''returns the total time required to complete the procedure'''
-        total_duration = 0
-        for action in self.actions:
-            total_duration += action[2]
-        return total_duration
 
     def end_procedure(self):
         '''This method must be run at the end of a procedure to free resources'''
@@ -104,15 +150,15 @@ class Resource:
 
         If qty_used == qty_total then the resource is not available
     '''
-    def __init__(self, resource_name, qty_total, qty_used):
+    def __init__(self, resource_name, qty_total):
         self.resource_name = resource_name
         self.qty_total = qty_total
-        self.qty_used = qty_used
+        self.qty_used = 0 # none used when initialized
 
     def is_available(self):
-        if (self.qty_used == self.qty_total):
-            return False
-        return True
+        if (self.qty_used < self.qty_total):
+            return True
+        return False
 
     def set_qty_total(self,qty):
         self.qty_total = qty
@@ -207,6 +253,12 @@ def schedule_model(tasks, staff):
     overlap_constraints(csp, task_vars)
     return csp, task_vars
 
+
+
+""" Constraints
+    1. Appointments cannot overlap
+    2. If appointment requires staff
+"""
 def add_position_contraints(csp, staff):
     ''' Add contraints that force staff to work together if they are
         under a certain level of power (i.e. trainees need experienced workers
